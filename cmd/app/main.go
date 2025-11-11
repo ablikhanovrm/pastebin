ae68a1249e0d3a85870326b3cd262574494f0cae
Rustem <ablikhanovrm@gmail.com>
2025-11-11 10:53:13 +0500

    wip: initial commit

---
+++ cmd/app/main.go
@@ -0,0 +1,9 @@
+package app
+
+import "fmt"
+
+const configPath = "configs/main.yaml"
+
+func main() {
+	fmt.Printf("helloo")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ablikhanovrm/pastebin
+
+go 1.25.0
