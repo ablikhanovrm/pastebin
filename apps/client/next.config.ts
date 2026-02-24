599d27361a6c8f5ec5fe54ae7d007a28c857125e
Rustem <ablikhanovrm@gmail.com>
2026-02-24 15:08:40 +0500

    wip: structure changes

--- apps/client/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-import { join } from "path";
-
-const nextConfig: NextConfig = {
-outputFileTracingRoot: join(__dirname),
-  distDir: '.next',
-};
-
-export default nextConfig;
